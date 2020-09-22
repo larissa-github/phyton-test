79a85227eece38786deb2a176f6518a37359b05e
larissa-github <70569583+larissa-github@users.noreply.github.com>
2020-09-22 17:08:58 +0300

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ echo "=> Larissa"
 
 # python hello1.py && python hello1.py
 # python hello1.py
-docker run -p 5000:5000 01254879/dock_rep_hello2_py:latest
+# docker run -p 5000:5000 01254879/dock_rep_hello2_py:latest
